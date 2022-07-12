0582e5ee41f1836cf9093473d10ca117953826c3
Hempfest <64885225+Hempfest@users.noreply.github.com>
2022-07-12 16:46:58 -0700

    * Remove items folder * Privatize Test class

--- panther-common/src/main/java/com/github/sanctum/panther/Test.java
+++ panther-common/src/main/java/com/github/sanctum/panther/Test.java
@@ -3,20 +3,17 @@ package com.github.sanctum.panther;
 import com.github.sanctum.panther.event.Vent;
 import com.github.sanctum.panther.event.VentMap;
 import com.github.sanctum.panther.file.JsonAdapter;
-import com.github.sanctum.panther.file.JsonConfiguration;
 import com.github.sanctum.panther.recursive.Service;
 import com.google.gson.JsonElement;
-import java.io.File;
 import java.util.Map;
 
 @Vent.Link.Key("Main class")
 public final class Test implements Vent.Host, JsonAdapter<Service> {
 
+	Test() {
+	}
+
 	public static void main(String[] args) {
-
-		File f = new File("items");
-
-		JsonConfiguration c = new JsonConfiguration(f, "data", null);
 
 
 	}
