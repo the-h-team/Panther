17ab97cf0d820a285f88e84dc14f72bebff8ffce
Matt <58913953+ms5984@users.noreply.github.com>
2022-11-08 23:09:45 -0500

    Configure shadow * Narrow scope of relocation to implementation dependency guava only. The logic here is that consumers of these artifacts will want and expect api configuration dependencies gson and json-simple at their original locations while guava represents an opportunity for relocation and minimization. * Change classifier to "bundled" to reflect mixed approach

--- buildSrc/src/main/kotlin/io/github/sanctum/panther/panther.external-shadow-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/io/github/sanctum/panther/panther.external-shadow-conventions.gradle.kts
@@ -4,20 +4,19 @@ plugins {
 }
 
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
-    archiveClassifier.set("relocated")
-
-    // Minimize guava anyway (see https://imperceptiblethoughts.com/shadow/configuration/minimizing/#:~:text=Dependencies%20scoped%20as%20api)
+    archiveClassifier.set("bundled")
+    // Minimize guava (see https://imperceptiblethoughts.com/shadow/configuration/minimizing/#:~:text=Dependencies%20scoped%20as%20api)
     minimize()
-
-    // Relocate gson, guava (what's left) and json-simple
-    // gson first
-    relocate("com.google.gson", "io.github.sanctum.panther.relocated.gson")
-    // then guava
-    relocate("com.google", "io.github.sanctum.panther.relocated.guava.google")
-    // guava -> jsr305:3.0.2
-    relocate("javax.annotation", "io.github.sanctum.panther.relocated.guava.jsr305")
-    // guava -> checker_qual:3.12.0
-    relocate("org.checkerframework", "io.github.sanctum.panther.relocated.guava.checker_qual")
-    // json-simple
-    relocate("org.json.simple", "io.github.sanctum.panther.relocated.json_simple")
+    val relocationPackage = "relocations.panther"
+    // Relocate what's left
+    relocate("com.google.common", "$relocationPackage.guava")
+    // guava ->depends error_prone_annotations:2.11.0
+    relocate("com.google.errorprone", "$relocationPackage.guava_error_prone_annotations")
+    // guava ->depends j2objc-annotations:1.3
+    relocate("com.google.j2objc", "$relocationPackage.guava_j2objc_annotations")
+    // guava ->depends jsr305:3.0.2
+    relocate("javax.annotation", "$relocationPackage.guava_jsr305")
+    // guava ->depends checker-qual:3.12.0
+    relocate("org.checkerframework", "$relocationPackage.guava_checker_qual")
+    // Leave gson and json-simple alone
 }
