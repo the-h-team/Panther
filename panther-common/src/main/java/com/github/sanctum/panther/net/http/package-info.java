54c8583428c4b18c5cff8a6c62fb9dc26aca45d6
Matt <58913953+ms5984@users.noreply.github.com>
2022-12-12 16:52:34 -0500

    package doc

--- panther-common/src/main/java/com/github/sanctum/panther/net/http/package-info.java
+++ panther-common/src/main/java/com/github/sanctum/panther/net/http/package-info.java
@@ -1,5 +1,9 @@
 /**
- * TODO doc
+ * Provides simple HTTP request and response handling.
+ * <p>
+ * Wraps Apache HttpClient.
+ *
  * @since 1.0.2
+ * @see com.github.sanctum.panther.net.http.HttpUtils
  */
 package com.github.sanctum.panther.net.http;
