d3a77f4529713c0c2bddb10fc0a3d4d77204bfe5
Matt <58913953+ms5984@users.noreply.github.com>
2023-03-30 14:02:10 -0400

    Set up Gradle Wrapper proper + upgrade * Upgrade Gradle version to 8.0.2 (-all) * Use wrapper task in root project build.gradle.kts to help hint to IntelliJ

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
+tasks.wrapper {
+    gradleVersion = "8.0.2"
+    distributionType = Wrapper.DistributionType.ALL
+}
+
 nexusPublishing {
     repositories {
         create("sonatype") {
