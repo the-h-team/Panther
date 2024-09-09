8befdbb1915eca093a0f4ebd461c43d3791af3ad
Matt <58913953+ms5984@users.noreply.github.com>
2024-09-09 00:36:13 -0400

    build: jdk resolver; versionCatalog wip
    * Add foojay-resolver plugin to automatically pull in a suitable jdk if one isn't provided by the host
    * Prep for version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,14 @@ sequenceOf(
     include(name)
     project(name).projectDir = file(path)
 }
+
+plugins {
+    // Enables easy resolution of a compatible jdk for subprojects
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        // TODO
+    }
+}
