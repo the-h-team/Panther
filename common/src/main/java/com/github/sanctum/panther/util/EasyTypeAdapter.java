9984c495b6cbbc5d63af723cb2903c9c6d469e0e
Hempfest <four20grow@gmail.com>
2025-05-25 22:58:23 -0700

    * Fix type adapter not being used right?

--- common/src/main/java/com/github/sanctum/panther/util/EasyTypeAdapter.java
+++ common/src/main/java/com/github/sanctum/panther/util/EasyTypeAdapter.java
@@ -2,16 +2,18 @@ package com.github.sanctum.panther.util;
 
 import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
+
 public class EasyTypeAdapter<T> implements TypeAdapter<T> {
 
-	private final TypeToken<T> token;
+	private final Type token;
 
 	public EasyTypeAdapter() {
-		this.token = new TypeToken<T>(){};
+		this.token = new TypeToken<T>(){}.getType();
 	}
 
 	@Override
 	public Class<T> getType() {
-		return (Class<T>) token.getRawType();
+		return (Class<T>) token.getClass();
 	}
 }
